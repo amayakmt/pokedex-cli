bfd114fa43ad8f8dcb764cc1d421ffbfb43845f4
amayakmt <amayak.151001@gmail.com>
2026-06-11 13:34:14 +0900

    Implement REPL + 2 commands (help & exit)

--- main.go
+++ main.go
@@ -1,9 +1,72 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	commands := getCommands()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Printf("Pokedex > ")
+		scanner.Scan()
+		words := cleanInput(scanner.Text())
+
+		if len(words) == 0 {
+			continue
+		}
+
+		command, ok := commands[words[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		err := command.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+	}
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Printf("Welcome to the Pokedex!\n")
+
+	fmt.Printf("Usage:\n\n")
+	for _, command := range getCommands() {
+		fmt.Printf("%v: %v\n", command.name, command.description)
+	}
+
+	return nil
 }
